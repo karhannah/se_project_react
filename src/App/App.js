8229004aba39c861b457d83e059fddfda363e789
XDRO <joshuaarcher76@gmail.com>
2023-11-02 14:24:58 -0500

    start on header

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,30 @@
-import logo from "./logo.svg";
 import ItemModal from "../ItemModal/ItemModal";
 import "./App.css";
+// image imports
+import headerLogo from "../images/headerLogo.svg";
+import AvatarLogo from "../images/headerAvatar.svg";
 
 function App() {
   return (
     <div>
-      <header className="headerLogo">
-        <div>Header Logo</div>
-        <div>Date</div>
-        <div>
-          <button type="text" />
+      <header className="header">
+        <div className="header__logo">
+          <div>
+            <img src={headerLogo} alt="Logo" />
+          </div>
+          <div>Date</div>
         </div>
-        <div>Name</div>
-        <div>Avatar Logo</div>
+        <div className="header__avatar-logo">
+          <div>
+            <button className="header__button-add-clothes" type="button">
+              + Add Clothes
+            </button>
+          </div>
+          <div>Terrence Tegegne</div>
+          <div>
+            <img src={AvatarLogo} alt="Avatar" />
+          </div>
+        </div>
       </header>
     </div>
   );
