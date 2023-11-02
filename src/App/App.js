130bf51de3becd70ed0ba2480129adee07b7d5bf
XDRO <joshuaarcher76@gmail.com>
2023-11-02 13:37:03 -0500

    move index outside of index folder

--- src/App/App.js
+++ src/App/App.js
@@ -4,20 +4,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="headerLogo">
+        <div>Header Logo</div>
+        <div>Date</div>
+        <div>
+          <button type="text" />
+        </div>
+        <div>Name</div>
+        <div>Avatar Logo</div>
       </header>
     </div>
   );
