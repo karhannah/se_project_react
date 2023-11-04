a2fd81338dc71ac3afeb9dbc2ca86bf3adf8d9e2
XDRO <joshuaarcher76@gmail.com>
2023-11-04 10:26:28 -0500

    uncomment weather api

--- src/Utils/WeatherApi.js
+++ src/Utils/WeatherApi.js
@@ -1,29 +1,29 @@
 import React from "react";
 import WeatherAPIEndPoint from "./WeatherApi";
 import WeatherAPIKey from "./WeatherApi";
-// import axios from "axios";
+import axios from "axios";
 
-// function Weather() {
-//   const [latitude, setLat] = React.useState("");
-//   const [longitude, setLong] = React.useState("");
-//   const [city, setCity] = React.useState("");
-//   React.useEffect(() => {
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       setLat(position.coords.latitude);
-//       setLong(position.coords.longitude);
-//     });
+function Weather() {
+  const [latitude, setLat] = React.useState("");
+  const [longitude, setLong] = React.useState("");
+  const [city, setCity] = React.useState("");
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
 
-//     let WeatherFinalAPIEndPoint = `${WeatherAPIEndPoint}lat=${latitude}&lon=${longitude}&appid=${WeatherAPIKey}`;
+    let WeatherFinalAPIEndPoint = `${WeatherAPIEndPoint}lat=${latitude}&lon=${longitude}&appid=${WeatherAPIKey}`;
 
-//     axios.get(WeatherFinalAPIEndPoint).then((response) => {
-//       setCity(city.data);
-//     });
-//   }, []);
-//   return (
-//     <div>
-//       <h1>{setCity.name}</h1>
-//     </div>
-//   );
-// }
+    axios.get(WeatherFinalAPIEndPoint).then((response) => {
+      setCity(city.data);
+    });
+  }, []);
+  return (
+    <div>
+      <h1>{setCity.name}</h1>
+    </div>
+  );
+}
 
-// export default Weather;
+export default Weather;
