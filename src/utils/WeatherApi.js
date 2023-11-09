3d89e7956a8d1530318afc7840b9a4e603dad464
XDRO <joshuaarcher76@gmail.com>
2023-11-09 14:18:59 -0600

    Add a functoin for processing server res wApi.js

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -4,11 +4,15 @@ export const getWeatherAndLocation = () => {
   const API = fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
   ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
+    const processServerResponse = (res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Error: ${res.status}`).then(
+          processServerResponse
+        );
+      }
+    };
   });
   return API;
 };
