6bcd3a8c0fd90a1d13617877a124370bbf14d764
XDRO <joshuaarcher76@gmail.com>
2023-11-28 14:53:25 -0600

    remove extra div tag in header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,39 +6,37 @@ import "./Header.css";
 
 const Header = ({ onCreate, city, currentDate }) => {
   return (
-    <div>
-      <header className="header">
-        <div className="header__logo">
-          <div>
-            <Link to="/">
-              <img src={avatarImage} alt="Logo" />
-            </Link>
-          </div>
-          <div className="header__date-and-location">
-            <div className="header__date">{currentDate},</div>
-            <div className="header__location">{city}</div>
-          </div>
-        </div>
-        <div className="header__avatar-logo">
-          <ToggleSwitch />
-          <div>
-            <button
-              className="header__button-add-clothes"
-              onClick={onCreate}
-              type="button"
-            >
-              + Add Clothes
-            </button>
-          </div>
-          <Link to="/profile" className="header__user-name">
-            Terrence Tegegne
+    <header className="header">
+      <div className="header__logo">
+        <div>
+          <Link to="/">
+            <img src={avatarImage} alt="Logo" />
           </Link>
-          <div>
-            <img src={AvatarLogo} alt="Avatar" />
-          </div>
         </div>
-      </header>
-    </div>
+        <div className="header__date-and-location">
+          <div className="header__date">{currentDate},</div>
+          <div className="header__location">{city}</div>
+        </div>
+      </div>
+      <div className="header__avatar-logo">
+        <ToggleSwitch />
+        <div>
+          <button
+            className="header__button-add-clothes"
+            onClick={onCreate}
+            type="button"
+          >
+            + Add Clothes
+          </button>
+        </div>
+        <Link to="/profile" className="header__user-name">
+          Terrence Tegegne
+        </Link>
+        <div>
+          <img src={AvatarLogo} alt="Avatar" />
+        </div>
+      </div>
+    </header>
   );
 };
 
