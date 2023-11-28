c41b252bb54fb46a92d343ac19c3ad0f8d592364
XDRO <joshuaarcher76@gmail.com>
2023-11-28 15:26:32 -0600

    change div inside of sideBar.js file to p tag

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,25 +3,23 @@ import ClothesSection from "./ClothesSection";
 import "./Profile.css";
 const Profile = ({ onCreate, clothingItems, onSelectCard }) => {
   return (
-    <div>
-      <div className="profile">
-        <SideBar />
-        <div className="profile__items-container">
-          <div className="profile__items-text">
-            Your Items
-            <button
-              className="profile__add-button"
-              onClick={onCreate}
-              type="button"
-            >
-              + Add new
-            </button>
-          </div>
-          <ClothesSection
-            clothingItems={clothingItems}
-            onSelectCard={onSelectCard}
-          />
+    <div className="profile">
+      <SideBar />
+      <div className="profile__items-container">
+        <div className="profile__items-text">
+          Your Items
+          <button
+            className="profile__add-button"
+            onClick={onCreate}
+            type="button"
+          >
+            + Add new
+          </button>
         </div>
+        <ClothesSection
+          clothingItems={clothingItems}
+          onSelectCard={onSelectCard}
+        />
       </div>
     </div>
   );
--- src/components/Profile/SideBar.js
+++ src/components/Profile/SideBar.js
@@ -4,7 +4,7 @@ const SideBar = () => {
   return (
     <div className="profile__logo">
       <img className="profile__avatar-logo" src={AvatarLogo} alt="Avatar" />
-      <div className="profile__user-name">Terrence Tegegne</div>
+      <p className="profile__user-name">Terrence Tegegne</p>
     </div>
   );
 };
