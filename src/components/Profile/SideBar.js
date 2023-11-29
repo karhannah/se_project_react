053955f77f3880b77ce15bf3f2bfcf8dbbcff340
XDRO <joshuaarcher76@gmail.com>
2023-11-29 10:14:25 -0600

    change AvatarLogo to avatarLogo sideBar.js

--- src/components/Profile/SideBar.js
+++ src/components/Profile/SideBar.js
@@ -1,9 +1,9 @@
-import AvatarLogo from "../../images/headerAvatar.svg";
+import avatarLogo from "../../images/headerAvatar.svg";
 
 const SideBar = () => {
   return (
     <div className="profile__logo">
-      <img className="profile__avatar-logo" src={AvatarLogo} alt="Avatar" />
+      <img className="profile__avatar-logo" src={avatarLogo} alt="Avatar" />
       <p className="profile__user-name">Terrence Tegegne</p>
     </div>
   );
