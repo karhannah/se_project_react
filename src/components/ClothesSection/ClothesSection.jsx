4fb4447f4d8bbc4b3f3b9c7502d1c9f0313a4189
Karinne Hannah <158782083+karhannah@users.noreply.github.com>
2024-12-30 11:49:29 -0500

    Update ClothesSection.jsx

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -5,26 +5,19 @@ const ClothesSection = ({ handleAddClick, onCardClick, clothingItems }) => {
 	return (
 		<div className = "clothes-section">
 			<div className = "clothes-section__wrapper">
-				<div class = "section__controls">
-					<p class = "controls__label">Your Items</p>
-					<button class = "controls__add-new" onClick={ handleAddClick }>+ Add New</button>
+				<div className = "section__controls">
+					<p className = "controls__label">Your Items</p>
+					<button className = "controls__add-new" onClick={ handleAddClick }>+ Add New</button>
 				</div>
 				<ul className="clothes-section__items card__container">
-					{clothingItems
-						.map((item) => {
+					{ clothingItems
+					  .toReversed().map((item) => {
 							return (
 								<ItemCard key={item._id}
 									item={item}
 									onCardClick={onCardClick}
 								/>);
-						})}
-					<div className = "newcard">
-						<ItemCard key = { -1 }
-								  cardId = { -1 }
-								  item = { { _id: -1, imageUrl: "", name: "", weather: "" } }
-								  onCardClick = { onCardClick }
-						/>
-					</div>
+						} ) }
 				</ul>
 			</div>
 		</div>
