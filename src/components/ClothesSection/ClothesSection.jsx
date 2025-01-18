83f45392e2d461740fe5428d97f1b670b50fc5e3
Karinne Hannah <158782083+karhannah@users.noreply.github.com>
2025-01-18 13:59:58 -0500

    Update ClothesSection.jsx

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,7 +1,9 @@
 import ItemCard from "../ItemCard/ItemCard";
 import "./ClothesSection.css";
+import React from "react";
 
 const ClothesSection = ({ handleAddClick, onCardClick, clothingItems }) => {
+	
 	return (
 		<div className = "clothes-section">
 			<div className = "clothes-section__wrapper">
@@ -10,14 +12,20 @@ const ClothesSection = ({ handleAddClick, onCardClick, clothingItems }) => {
 					<button className = "controls__add-new" onClick={ handleAddClick }>+ Add New</button>
 				</div>
 				<ul className="clothes-section__items card__container">
-					{ clothingItems
-					  .toReversed().map((item) => {
-							return (
-								<ItemCard key={item._id}
-									item={item}
-									onCardClick={onCardClick}
-								/>);
-						} ) }
+					{ clothingItems.filter((item) => {
+						return true;
+					} ).reverse().map((item) => {
+
+						item.cardRef = React.createRef();
+
+						return (
+							<ItemCard cardRef={ item.cardRef }
+								key={item._id}
+								cardId={item._id}
+								item={item}
+								onCardClick={onCardClick}
+							/>);
+					  } ) }
 				</ul>
 			</div>
 		</div>
